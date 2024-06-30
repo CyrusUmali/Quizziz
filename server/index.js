56ca37e0188208ca6c7275add009727e2502dfda
Cyrus U. Carbungco <144579806+CyrusUmali@users.noreply.github.com>
2024-06-30 18:19:31 +0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const app = express();
 
 // CORS configuration
 const corsOptions = {
-  origin: 'http://localhost:5173',  // Adjust the origin to your frontend's URL
+  origin: 'https://quizziz-client-fsez.onrender.com/',  // Adjust the origin to your frontend's URL
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,
   optionsSuccessStatus: 204,
