344e86191c3aad526dd13a69f7bf3ee9c19137f4
Cyrus U. Carbungco <144579806+CyrusUmali@users.noreply.github.com>
2024-06-30 19:34:02 +0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const db = require("../connect");
+const db = require("./connect");
 const jwt = require('jsonwebtoken');
 const { OAuth2Client } = require('google-auth-library');
 const client = new OAuth2Client('460297050109-vg8lujpfnftm1bafbqsur8drtiv8tdic.apps.googleusercontent.com');
-const authenticateToken = require('../middleware/authenticateToken');
+const authenticateToken = require('./middleware/authenticateToken');
 // const { register } = require('../controllers/auth'); 
 
 const router = express.Router();
