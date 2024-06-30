e5ee711d90912ac50815ad9cad56e106fa8eb267
Cyrus U. Carbungco <144579806+CyrusUmali@users.noreply.github.com>
2024-06-30 12:45:10 +0800

    Update connect.js

--- server/connect.js
+++ server/connect.js
@@ -1,10 +1,17 @@
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "umalic65",
-  database: "quizziz"
+  // host: "localhost",
+  // user: "root",
+  // password: "umalic65",
+  // database: "quizziz"
+
+ host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  port: process.env.MYSQL_PORT || 3306
+  
 });
 
 db.connect((err) => {
